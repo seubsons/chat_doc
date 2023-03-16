9c780be1506175a4f662845dca64eb6f078fc8ff
seubsons <seubsons@gmail.com>
2023-03-16 19:49:07 +0000

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if 'past' not in st.session_state:
     st.session_state['past'] = []
 
 def get_text():
-    input_text = st.text_input('You: ','Hello, How are you?', key='input')
+    #input_text = st.text_input('You: ','Hello, How are you?', key='input')
     input_text = st.text_input('','How Can I help You?')
     return input_text
 
