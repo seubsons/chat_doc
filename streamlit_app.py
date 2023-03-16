0638c680f8971ba2dcedc7a7877deb810ade6d47
seubsons <seubsons@gmail.com>
2023-03-16 19:27:07 +0000

    test chat_doc commit

---
+++ streamlit_app.py
@@ -0,0 +1,47 @@
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+openai.api_key = st.secrets["pass"]
+
+def generate_response(prompt):
+    
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.3
+    )
+    
+    message = response.choices[0].text
+    
+    return message
+
+st.title('Welcome to Doctor Appointment Center')
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+def get_text():
+    #input_text = st.text_input('You: ','Hello, How are you?', key='input')
+    input_text = st.text_input('You: ','Hello, can I ask you a question?', key='input')
+    return input_text
+
+user_input = get_text()
+
+if user_input:
+    output = generate_response(user_input)
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state['generated'][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
+
